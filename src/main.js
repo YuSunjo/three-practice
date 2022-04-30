92a8ae4750b81eadca18cac2f0f812c9ac704ff0
YuSunjo <tnswh2023@gmail.com>
2022-04-30 11:19:42 +0900

    OrthographicCamera

--- src/main.js
+++ src/main.js
@@ -19,17 +19,33 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 const scene = new THREE.Scene();
 // 카메라
 // https://threejs.org/docs/index.html?q=came#api/ko/cameras/PerspectiveCamera
-const camera = new THREE.PerspectiveCamera(
-    75,
+// const camera = new THREE.PerspectiveCamera(
+//     75,
+//     window.innerWidth / window.innerHeight,
+//     0.1,
+//     1000
+// )
+// // z 축 (미터 단위)
+// camera.position.x = 1;
+// camera.position.y = 2;
+// camera.position.z = 5;
+// scene.add(camera);
+
+const camera = new THREE.OrthographicCamera(
+    -(window.innerWidth / window.innerHeight),
     window.innerWidth / window.innerHeight,
+    1,
+    -1,
     0.1,
     1000
 )
-// z 축 (미터 단위)
 camera.position.x = 1;
 camera.position.y = 2;
 camera.position.z = 5;
-scene.add(camera);
+camera.lookAt(0, 0, 0);
+camera.zoom = 0.5;
+camera.updateProjectionMatrix();
+scene.add(camera)
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
