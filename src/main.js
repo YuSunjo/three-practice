b62eeb3e876cf8a3b317c69f6f25ba553a9c0da4
YuSunjo <tnswh2023@gmail.com>
2022-04-30 11:12:30 +0900

    PerspectiveCamera를 이용해서 scene 그려주기

--- src/main.js
+++ src/main.js
@@ -1,28 +1,42 @@
-import TextSign from './TextSign.js';
+import * as THREE from 'three'
 
-const textSign = new TextSign();
+// 동적 캔버스 조립
+// console.log(THREE)
+// const renderer = new THREE.WebGLRenderer()
+//
+// renderer.setSize(window.innerWidth, window.innerHeight)
+//
+// console.log(renderer.domElement);
+// document.body.appendChild(renderer.domElement)
 
-const condition = true;
-// const condition = false;
-
-const promise = new Promise((resolve, reject) => {
-	if (condition) {
-		resolve('성공!');
-	} else {
-		reject('실패!');
-	}
+const canvas = document.querySelector('#three-canvas');
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true
 });
-promise
-	.then(message => {
-		console.log(message);
-		textSign.elem.innerHTML = message;
-		textSign.elem.dataset.state = 'success';
-	})
-	.catch(error => {
-		console.log(error);
-		textSign.elem.innerHTML = error;
-		textSign.elem.dataset.state = 'failure';
-	})
-	.finally(() => {
-		console.log('끝에 실행');
-	});
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+const scene = new THREE.Scene();
+// 카메라
+// https://threejs.org/docs/index.html?q=came#api/ko/cameras/PerspectiveCamera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+// z 축 (미터 단위)
+camera.position.x = 1;
+camera.position.y = 2;
+camera.position.z = 5;
+scene.add(camera);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh)
+
+// 그리기
+renderer.render(scene, camera);
